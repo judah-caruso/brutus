75ed9774a77f44880290edce78baa9a894e40af3
Judah Caruso <judah@tuta.io>
2024-08-23 21:58:00 -0600

    flto on mac

--- build_mac.sh
+++ build_mac.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-clang -o brutus -std=c99 -I./src -I./src/luajit/src -L./src/luajit/src -lluajit -Wall -Werror -fpic main.c
+clang -o brutus -std=c99 -I./src -I./src/luajit/src -L./src/luajit/src -lluajit -Wall -Werror -fpic -flto main.c
